749f9ef38dfa1410cae66253e3ba1279cf990314
unknown <andres.arias-a@labinfo.is.escuelaing.edu.co>
2024-08-29 15:54:01 -0500

    1.0

--- src/main/java/org/example/HelloService.java
+++ src/main/java/org/example/HelloService.java
@@ -7,4 +7,23 @@ public class HelloService {
     public static String hello(){
         return "Hello world";
     }
+
+    @GetMapping("/Pi")
+    public static double Pi(){
+        return Math.PI;
+    }
+
+    @GetMapping("/funciones")
+    public static  String[] funciones(){
+        String[] funciones = {"coseno","seno","tangente"};
+        return funciones;
+    }
+
+    @GetMapping("/estudiantes")
+    public static String[] estudiantes(){
+        String[] estudiantes = {"daniel","andres","sergio"};
+        return estudiantes;
+    }
+
+
 }
--- src/main/java/org/example/Spring.java
+++ src/main/java/org/example/Spring.java
@@ -10,21 +10,21 @@ import java.util.Map;
 public class Spring {
     public static void main(String[] args) throws ClassNotFoundException, MalformedURLException, InvocationTargetException, IllegalAccessException {
         Class c = Class.forName(args[0]);
-        Map<String,Method> services = new HashMap<>();
+        Map<String, Method> services = new HashMap<>();
         Method[] methods;
 
         //Uploading components
-        if(c.isAnnotationPresent(RestController.class)){
+        if (c.isAnnotationPresent(RestController.class)) {
             methods = c.getDeclaredMethods();
-            for(Method m : methods){
-                if(m.isAnnotationPresent(GetMapping.class)){
+            for (Method m : methods) {
+                if (m.isAnnotationPresent(GetMapping.class)) {
                     String key = m.getAnnotation(GetMapping.class).value();
-                    services.put(key,m);
+                    services.put(key, m);
                 }
             }
         }
 
-        //burned code for a example practice
+        //burned code for a example practice hello
         URL serviceUrl = new URL("http://localhost:8080/App/hello");
         String path = serviceUrl.getPath();
         System.out.println("PATH ------->" + path);
@@ -33,6 +33,43 @@ public class Spring {
         System.out.println("Service Name ------> " + serviceName);
 
         Method m = services.get(serviceName);
-        System.out.println(m.invoke(null));
+        System.out.println("hello world" + m.invoke(null));
+
+        //pi
+        URL serviceUrl1 = new URL("http://localhost:8080/App/Pi");
+        String path1 = serviceUrl1.getPath();
+        System.out.println("PATH ------->" + path1);
+
+        String serviceName1 = path1.substring(4);
+        System.out.println("Service Name ------> " + serviceName);
+
+        Method m1 = services.get(serviceName1);
+        System.out.println("pi" + m1.invoke(null));
+
+
+        // funciones
+        URL serviceUrl2 = new URL("http://localhost:8080/App/funciones");
+        String path2 = serviceUrl2.getPath();
+        System.out.println("PATH ------->" + path2);
+
+        String serviceName2 = path.substring(4);
+        System.out.println("Service Name ------> " + serviceName2);
+
+        Method m2 = services.get(serviceName);
+        System.out.println("funciones" + m2.invoke(null));
+
+
+        // Estudiantes
+        URL serviceUrl3 = new URL("http://localhost:8080/App/estudiantes");
+        String path3 = serviceUrl3.getPath();
+        System.out.println("PATH ------->" + path3);
+
+        String serviceName3 = path.substring(4);
+        System.out.println("Service Name ------> " + serviceName3);
+
+        Method m3 = services.get(serviceName);
+        System.out.println("estudiantes" + m3.invoke(null));
+
+
     }
 }
